f402f8a9543cdfa5dd1cac0c878c7caefd8e2e4e
Diogo Sequeira <diogosequeira@hotmail.fr>
2024-02-01 10:23:55 +0000

    Update CharacterPersonalPage.java

--- app/src/main/java/com/example/maytheforcebewith_diogosequeira/Fragments/CharacterPersonalPage.java
+++ app/src/main/java/com/example/maytheforcebewith_diogosequeira/Fragments/CharacterPersonalPage.java
@@ -1,10 +1,8 @@
 package com.example.maytheforcebewith_diogosequeira.Fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,42 +10,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.maytheforcebewith_diogosequeira.R;
 import com.example.maytheforcebewith_diogosequeira.Retrofit.CharacterRecycler;
 import com.example.maytheforcebewith_diogosequeira.Utils.FavouritesFragment;
 
-
 public class CharacterPersonalPage extends Fragment {
-
     private TextView name, eye, birth, mass, height, skin, gender, hair, homeworld;
     private ImageView favouriteIcon, backButton;
     private Button sendPost;
     private boolean isToggle;
     private String novoNome;
 
-    public CharacterPersonalPage() {
-        // Required empty public constructor
-    }
-
-
-
+    public CharacterPersonalPage() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_people_personal, container, false);
-
-
-
         initViews(v);
         bundleSetters();
 
         favouriteIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if(!isToggle){
                     favouriteIcon.setImageResource(R.drawable.ic_star_black_24dp);
                     Toast.makeText(getContext(), "Added to your favourites!", Toast.LENGTH_SHORT).show();
@@ -55,18 +41,13 @@ public class CharacterPersonalPage extends Fragment {
                     // I need to get all the favourite Chars in Favourites Fragment and display them in a list
 
                     if(!sameName(novoNome)){
-
                         FavouritesFragment.getFavourists().add(new CharacterRecycler(R.drawable.yoda, novoNome));
                         System.out.println(novoNome);
                         System.out.println(FavouritesFragment.getFavourists().size());
                         isToggle = true;
-
                     } else {
-
                         Toast.makeText(getContext(), "Already added before!", Toast.LENGTH_SHORT);
                     }
-
-
                 } else {
                     favouriteIcon.setImageResource(R.drawable.ic_star_border_black_24dp);
                     Toast.makeText(getContext(), "Removed from your favourites", Toast.LENGTH_SHORT).show();
@@ -85,12 +66,9 @@ public class CharacterPersonalPage extends Fragment {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
             backTransaction();
             }
         });
-
-
         return v;
     }
 
@@ -110,16 +88,10 @@ public class CharacterPersonalPage extends Fragment {
     }
 
     private void bundleSetters(){
-
         Bundle bundle = getArguments();
-
         if(bundle != null){
-
-
             //Here I check if bundle is coming via search or recyclerView index
-
                 if(bundle.size() == 2){
-
                     String nome = getArguments().getString("newname");
                     name.setText(nome);
 
@@ -127,9 +99,7 @@ public class CharacterPersonalPage extends Fragment {
                     birth.setText("Full details:\n\n" + details);
 
                     invisible();
-
                 } else {
-
                     String nome = getArguments().getString("Titulo");
                     novoNome = getArguments().getString("Titulo");
                     name.setText(nome);
@@ -158,24 +128,17 @@ public class CharacterPersonalPage extends Fragment {
                     String homeChar = getArguments().getString("homeworld");
                     homeworld.setText("Home: " + homeChar);
                 }
-
         }
-
-
     }
-
-
+    
     private void backTransaction(){
-
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.replace(R.id.main_container, new ListFragment());
         fragmentTransaction.commit();
-
     }
 
     private void invisible(){
-
         eye.setVisibility(View.GONE);
         mass.setVisibility(View.GONE);
         height.setVisibility(View.GONE);
@@ -183,29 +146,18 @@ public class CharacterPersonalPage extends Fragment {
         gender.setVisibility(View.GONE);
         hair.setVisibility(View.GONE);
         homeworld.setVisibility(View.GONE);
-
     }
-
-
+    
     // Method to check if there is an object with same name on the ArrayList
     private boolean sameName(String name){
-
         if(FavouritesFragment.getFavourists().size() != 0){
             for(int i = 0; i < FavouritesFragment.getFavourists().size(); i++){
-
                 if(FavouritesFragment.getFavourists().get(i).getName().equals(name)){
-
                     System.out.println("It has the same name");
                     return true;
                 }
             }
         }
-
-
         return false;
     }
-
-
-    }
-
-
+}
