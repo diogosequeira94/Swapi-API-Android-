4544b4e05e01bd2357f3a7586fc8a7a4c4a473f1
Diogo Sequeira <diogosequeira@hotmail.fr>
2024-02-01 10:25:06 +0000

    Update HomeFragment.java

--- app/src/main/java/com/example/maytheforcebewith_diogosequeira/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/maytheforcebewith_diogosequeira/Fragments/HomeFragment.java
@@ -1,20 +1,15 @@
 package com.example.maytheforcebewith_diogosequeira.Fragments;
 
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.example.maytheforcebewith_diogosequeira.R;
 import com.example.maytheforcebewith_diogosequeira.Utils.FavouritesFragment;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -23,68 +18,42 @@ import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
-
-    Button character, favourites;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-
-
-
+ Button character, favourites;
+     
+    public HomeFragment() {}
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-
         View v =  inflater.inflate(R.layout.fragment_home, container, false);
         character = v.findViewById(R.id.button1);
         favourites = v.findViewById(R.id.button2);
 
         character.setOnClickListener(this);
         favourites.setOnClickListener(this);
-
-
-
         return v;
-
     }
-
 
     @Override
     public void onClick(View view) {
-
         switch(view.getId()){
-
             case R.id.button1:
-
-                //User needs to have internet enabled before fragmentTransaction
-
                 if(isOnline()){
-
                     FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.replace(R.id.main_container, new ListFragment());
                     fragmentTransaction.commit();
-
                 } else {
-
                     Toast.makeText(getContext(), "Please enable Internet access", Toast.LENGTH_SHORT).show();
                 }
                 break;
-
             case R.id.button2:
-
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.replace(R.id.main_container, new FavouritesFragment());
                 fragmentTransaction.commit();
-
                 break;
         }
-
     }
 
     public boolean isOnline() {
@@ -92,20 +61,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         try {
             Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
             int exitValue = ipProcess.waitFor();
-
             return (exitValue == 0);
-
         } catch (IOException e) {
             e.printStackTrace();
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         return false;
-
     }
-
-
-
 }
